f07e0cf20973d732adae674370bd4d877a147fd1
Potato <discord.siesta.me@gmail.com>
2025-01-20 10:44:02 +0600

    Update Crisp.tsx

--- components/Crisp.tsx
+++ components/Crisp.tsx
@@ -1,13 +1,20 @@
-"use client";
-//@ts-ignore
+"use client"; 
 
 import React, { useEffect } from "react";
-import { Crisp } from "crisp-sdk-web";
 
 const CrispApp = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      Crisp.configure(`91af6372-1645-4b6b-aac0-f5614d04808c`);
+      window.$crisp = [];
+      window.CRISP_WEBSITE_ID = "91af6372-1645-4b6b-aac0-f5614d04808c";
+
+      (function () {
+        const d = document;
+        const s = d.createElement("script");
+        s.src = "https://client.crisp.chat/l.js";
+        s.async = true;
+        d.getElementsByTagName("head")[0].appendChild(s);
+      })();
     }
   }, []);
 
