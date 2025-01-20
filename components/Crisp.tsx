cebb70f6af7c6f1e6bed7c61b57c06ee51a16bbc
Potato <discord.siesta.me@gmail.com>
2025-01-20 10:28:52 +0600

    Update Crisp.tsx

--- components/Crisp.tsx
+++ components/Crisp.tsx
@@ -7,7 +7,7 @@ import { Crisp } from "crisp-sdk-web";
 const CrispApp = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      Crisp.configure(`3984329d-3c67-491c-adf0-bcc896d4d2fe`);
+      Crisp.configure(`b3ec299b-cc0b-4410-9ab7-e4b52b5144b7`);
     }
   }, []);
 
